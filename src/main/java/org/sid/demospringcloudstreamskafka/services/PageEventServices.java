7edcf2c7454b162d184271546aefdc8bbee0eea6
saidnidmessaoud <nidmessaoudsaid1@gmail.com>
2024-01-17 00:58:32 +0000

    V2

--- src/main/java/org/sid/demospringcloudstreamskafka/services/PageEventServices.java
+++ src/main/java/org/sid/demospringcloudstreamskafka/services/PageEventServices.java
@@ -1,9 +1,16 @@
 package org.sid.demospringcloudstreamskafka.services;
 
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.sid.demospringcloudstreamskafka.entities.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
@@ -34,6 +41,21 @@ public class PageEventServices {
             input.setName("L:"+input.getName().length());
             input.setUser("UUUUUU");
             return input;
+        };
+    }
+
+
+    @Bean
+    public Function<KStream<String,PageEvent>,KStream<String,Long>> kStreamFunction(){
+        return (input)->{
+            return input
+                    .filter((k,v)->v.getDuration()>100)
+                    .map((k,v)->new KeyValue<>(v.getName(),0L))
+                    .groupBy((k,v)->k, Grouped.with(Serdes.String(),Serdes.Long()))
+                    .windowedBy(TimeWindows.of(Duration.ofSeconds(5)))
+                    .count(Materialized.as("page-count"))
+                    .toStream()
+                    .map((k,v)->new KeyValue<>("=>"+k.window().startTime()+k.window().endTime()+k.key(),v));
         };
     }
 
